5649fc37946183d4256c22cfa089c00d79629cb5
CammiePone <inazuma261@gmail.com>
2020-12-15 21:46:23 -0500

    Removed unnecessary shadows in LivingEntityMixn

--- src/main/java/dev/camscorner/camsarmoury/core/mixin/LivingEntityMixin.java
+++ src/main/java/dev/camscorner/camsarmoury/core/mixin/LivingEntityMixin.java
@@ -7,22 +7,16 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity
 {
-	@Shadow public abstract ItemStack getStackInHand(Hand hand);
-
-	@Shadow public boolean handSwinging;
-
 	public LivingEntityMixin(EntityType<?> type, World world) { super(type, world); }
 
 	@ModifyVariable(method = "applyDamage", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/entity/LivingEntity;applyArmorToDamage(Lnet/minecraft/entity/damage/DamageSource;F)F", ordinal = 0))
